2fe22f85edac6ef9d37176d8dc8022dfe815bc33
Hyeongjin Kim <hhjj0506@gmail.com>
2022-07-02 20:44:31 +0900

    working on crud

--- src/Firebase.js
+++ src/Firebase.js
@@ -13,6 +13,7 @@ import {
  where,
  addDoc,
 } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBQWJexhoJvZ6bc22Sm4oR3f2ITnb9WI6w",
@@ -27,6 +28,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 const googleProvider = new GoogleAuthProvider();
 const signInWithGoogle = async () => {
   try {
@@ -34,22 +36,6 @@ const signInWithGoogle = async () => {
     const user = res.user;
     if (!user.email.includes("handong.ac.kr")) {
         return -1;
-    }
-    const q = db.collection("users").doc(user.uid);
-    const docs = await getDocs(q);
-    if (docs.docs.length === 0) {
-      await db.collection("users").doc(user.uid).set({
-        uid: user.uid,
-        name: user.displayName,
-        email: user.email,
-        nickname: null,
-        major: null,
-        profilePic: null,
-        content: null,
-        squat: 0,
-        deadlift: 0,
-        bench: 0,
-      });
     }
   } catch (err) {
     console.error(err);
@@ -60,5 +46,6 @@ const signInWithGoogle = async () => {
 export {
     auth,
     db,
+    storage,
     signInWithGoogle,
 };
--- src/Profile.js
+++ src/Profile.js
@@ -3,18 +3,41 @@ import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "./Firebase";
-import { signOut } from "firebase/auth";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import { signOut, updateProfile } from "firebase/auth";
+import { query, collection, getDocs, where, doc, setDoc, orderBy, onSnapshot, QuerySnapshot } from "firebase/firestore";
 import { Avatar, Grid, IconButton, Menu, Paper, MenuItem, Divider, CircularProgress, Button } from "@mui/material";
 import Navbar from "./components/Navbar";
 
 function Profile() {
     const [user, loading, error] = useAuthState(auth);
+    const [userInfo, setUserInfo] = useState([]);
     const navigate = useNavigate();
+    
+    const updateUserInfo = async (e) => {
+        e.preventDefault();
+        try {
+            await setDoc(doc(db, "users", user.uid), {
+                desc: desc,
+                
+            })
+            updateProfile(user, {
+                displayName: name,
+                photoURL: 
+            }).then(() => {
+                console.log("good");
+            })
+        }
+    }
 
     useEffect(() => {
         if (loading) return <CircularProgress/>;
         if (!user) return navigate("/");
+        const q = query(collection(db, "users"));
+        onSnapshot(q, (querySnapshot) => {
+            setUserInfo(querySnapshot.docs.map(doc => ({
+                data: doc.data()
+            })))
+        })
     }, [user, loading]);
 
     return (
@@ -22,7 +45,7 @@ function Profile() {
             <Container>
                 <Navbar/>
                 <Paper elevation={3}>
-                    <Avatar />
+                    <Avatar src={user?.photoURL} />
                 </Paper>
             </Container>
         </body>
