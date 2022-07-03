cdca8ad19d9dce5a3b86351b99f40535893901ce
Hyeongjin Kim <hhjj0506@gmail.com>
2022-07-03 15:20:23 +0900

    added create&update functions

--- src/Profile.js
+++ src/Profile.js
@@ -2,8 +2,9 @@ import { Container } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import { auth, db } from "./Firebase";
+import { auth, db, storage } from "./Firebase";
 import { signOut, updateProfile } from "firebase/auth";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { query, collection, getDocs, where, doc, setDoc, orderBy, onSnapshot, QuerySnapshot } from "firebase/firestore";
 import { Avatar, Grid, IconButton, Menu, Paper, MenuItem, Divider, CircularProgress, Button } from "@mui/material";
 import Navbar from "./components/Navbar";
@@ -11,22 +12,49 @@ import Navbar from "./components/Navbar";
 function Profile() {
     const [user, loading, error] = useAuthState(auth);
     const [userInfo, setUserInfo] = useState([]);
+    const [name, setName] = useState("");
+    const [openUserModal, setOpenUserModal] = useState(false);
+    const [desc, setDesc] = useState("");
+    const [imgURL, setImgURL] = useState("");
     const navigate = useNavigate();
     
-    const updateUserInfo = async (e) => {
+    // 이름과 사진을 제외한 프로필의 모든 정보
+    const updateProfileInfo = async (e) => {
         e.preventDefault();
         try {
             await setDoc(doc(db, "users", user.uid), {
                 desc: desc,
                 
             })
-            updateProfile(user, {
-                displayName: name,
-                photoURL: 
-            }).then(() => {
-                console.log("good");
-            })
+        } catch (err) {
+            alert(err)
         }
+    }
+
+    // 사진과 이름 바꾸기
+    // 사진은 storage 저장 후 링크를 photoURL로 설정, 이름은 displayName으로 설정
+    const updateUserInfo = (e) => {
+        e.preventDefault();
+        const file = e.target[0]?.files[0];
+        if (!file) return;
+        const storageRef = ref(storage, 'profile/${user?.photoURL}');
+        const upload = uploadBytesResumable(storageRef, file);
+
+        upload.on("state_changed",
+            (error) => { alert(error); },
+            () => {
+                getDownloadURL(upload.snapshot.ref).then((downloadURL) => {
+                    setImgURL(downloadURL);
+                });
+            }
+        );
+
+        updateProfile(user, {
+            displayName: name,
+            photoURL: imgURL,
+        }).then(() => {
+            console.log("good");
+        })
     }
 
     useEffect(() => {
@@ -46,6 +74,11 @@ function Profile() {
                 <Navbar/>
                 <Paper elevation={3}>
                     <Avatar src={user?.photoURL} />
+                    <Button onClick={() => setOpenUserModal(true)}>유저정보 수정</Button>
+                    <p>{user?.displayName}</p>
+                    <h3>소개</h3>
+                    <p>{}</p>
+                    <h3>경력</h3>
                 </Paper>
             </Container>
         </body>
